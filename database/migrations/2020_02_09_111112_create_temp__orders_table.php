e8cb1960a541e61ee7e3b1f3031893c1706f8d11
Kawsarul Alam Chowdhury Rifat <rifatron999@gmail.com>
2020-02-09 18:38:52 +0600

    offer 18 by rifat01

---
+++ database/migrations/2020_02_09_111112_create_temp__orders_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTempOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('temp__orders', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('customer_id');
+            $table->integer('shipping_id');
+            $table->integer('vendor_id');
+            $table->string('invoice_id')->nullable();
+            $table->string('product_ids')->nullable();
+            $table->string('selling price')->nullable();
+            $table->string('quantity')->nullable();
+            $table->string('offer_type')->nullable();
+            $table->string('offer_percentage')->nullable();
+            $table->string('free_product_ids')->nullable();
+            $table->integer('subtotal')->nullable();
+            $table->integer('total')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('temp__orders');
+    }
+}
